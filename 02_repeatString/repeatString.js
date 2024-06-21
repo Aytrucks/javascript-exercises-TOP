3a7f8de0c7e77564edbbddf207d43c04b1d3a5d0
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-06-21 16:26:04 -0700

    Complete two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let fullstring = "";
+    for(let i = 0;i<num;i++){
+        fullstring = fullstring + word;
+    }
+    return fullstring;
 };
 
 // Do not edit below this line
