9ab48002d7617bdac9b528ae6a0ebd96f6404567
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-06-24 12:02:57 -0700

    Finish exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(toBeRev) {
+    finishString = "";
+    
+    for(let i = toBeRev.length-1;i >= 0;i--){
+        finishString += toBeRev[i];
+    }
+    return finishString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    
+    argIndex = 0;
+    while(argIndex < arg.length){
+        for(let i = 0;i < array.length;i++){
+            if(array[i] === arg[argIndex]){
+                array.splice(i,1);
+                i--;
+            }
+        }
+        argIndex += 1;
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
