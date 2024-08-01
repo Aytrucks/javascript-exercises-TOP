c150d2fc28312b7c7aa8a3c3be4fba3962bc5d1f
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-01 14:53:05 -0700

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    let fiboSwitch = true;
+    let sum = 0;
+    
+    if(num < 0){
+        return "OOPS";
+    }
+    else if(num == 0){
+        return 0;
+    }
 
+    for(let i = 2;i < num;i++){
+        sum = a+b;
+        console.log(sum);
+        if(fiboSwitch){
+            fiboSwitch = !fiboSwitch;
+            a = sum;
+        }
+        else{
+            fiboSwitch = !fiboSwitch
+            b = sum;
+        }
+    }
+    return fiboSwitch ? b : a;
 };
 
 // Do not edit below this line
