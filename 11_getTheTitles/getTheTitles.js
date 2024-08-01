2c8181d90e62ee3cae107e0238ce9d5257ab8fdc
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-01 15:00:11 -0700

    Finish 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map((item) =>{
+        return item.title;
+    })
+    return titles;
 };
 
 // Do not edit below this line
