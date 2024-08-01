ff0699cc075bf0f1ac97ed8fb87f3acb6cac7181
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-01 14:01:41 -0700

    Finish 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (original) {
+    let newStr = "";
 
+    function isAlnum(string){
+        let code = 0;
+        let newStr = "";
+        for(let i = 0;i < string.length;i++){
+            code = string.charCodeAt(i);
+            if((code > 47 && code < 58) || (code > 64 && code < 91) || (code > 96 && code < 123)){
+                newStr += string[i];
+            }
+        }
+        return newStr;
+    }
+    let original2 = isAlnum(original);
+    for(let i = original2.length - 1;i >= 0;i--){
+        newStr += original2[i];
+    }
+    return newStr.toLowerCase() === original2.toLowerCase();
 };
 
 // Do not edit below this line
