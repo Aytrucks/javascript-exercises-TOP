dadbf0ee9c910b964793de2b0c71788d566ea092
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-01 13:39:39 -0700

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const theSum = arr.reduce((total,current) => {
+    return total += current;
+  },0);
+  return theSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const theSum = arr.reduce((total,current) => {
+    return total *= current;
+  },1);
+  return theSum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let powerResult = 1;
+  for(let i = 0;i < b;i++){
+    powerResult *= a;
+  }
+  return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factResult = 1;
+  let copynum = num;
+  if(num != 0){
+    for(let i = 0;i < num;i++){
+      factResult = factResult * copynum;
+      copynum--;
+    }
+  }
+  else{
+    return 1;
+  }
+  return factResult;
 };
 
 // Do not edit below this line
