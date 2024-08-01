9d52631f2e6b99100ebf76b9644ea44de0da0f79
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-01 15:21:26 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const today = new Date();
+    const sorted = array.sort((a,b) => {
+       if(!a.yearOfDeath){
+        a.yearOfDeath = today.getFullYear();
+       }
+       if(!b.yearOfDeath){
+        b.yearOfDeath = today.getFullYear();
+       }
+        return (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1
+    });
+    console.log(sorted);
+    return sorted[0];
 };
 
 // Do not edit below this line
